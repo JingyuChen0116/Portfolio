94b439df27d3805c402c7078eb6220e0974729e0
JingyuChen0116 <102499110+JingyuChen0116@users.noreply.github.com>
2023-10-29 20:13:48 +1100

    about-me page and skill page update

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 import './style.scss'
-import 'normalize.css'
 
-console.log('hello world');
+import { skills, projects } from "./data/db.json"
 
-window.onload
+/* Skill Grid */
+const grid_container = document.querySelector('.grid-container')
+
+skills.forEach( (skill) => {
+  const grid_item = document.createElement('div')
+  grid_item.className = 'grid-item'
+  grid_item.innerHTML = `
+    <div class='skill-header'>
+      <img/>
+      <span></span>
+    </div>
+    <p></p>
+  `
+
+  grid_item.querySelector('img').alt = skill.name
+  grid_item.querySelector('img').src = skill.src
+  grid_item.querySelector('span').textContent = skill.name
+  grid_item.querySelector('p').textContent = skill.description
+  
+  grid_container.appendChild(grid_item)
+})
